238a98cba2e5dc50ea9d9a0cd94dc12fd6da16ef
rulo64 <a_ponce@live.com.ar>
2021-11-16 20:17:56 -0300

    Falta pantalla de busqueda

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,7 +1,6 @@
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
-
 var app = new Framework7({
   // App root element
   root: '#app',
@@ -15,146 +14,68 @@ var app = new Framework7({
   },
   // Add default routes
   routes: [
-    {
-      path: '/login/',
-      url: 'login.html',
-    },
-    {
-      path: '/index/',
-      url: 'index.html',
-    },
-    {
-      path: '/registro/',
-      url: 'registro.html',
-    },
-    {
-      path: '/confirmacion/',
-      url: 'confirmacion.html',
-    },
-    {
-      path: '/errorreg/',
-      url: 'errorreg.html',
-    },
-    {
-      path: '/errorlog/',
-      url: 'errorlog.html',
-    },
-    {
-      path: '/datosperf/',
-      url: 'datosperf.html',
-    },
-    {
-      path: '/regdev/',
-      url: 'regdev.html',
-    },
-    {
-      path: '/regrec/',
-      url: 'regrec.html',
-    },
-    {
-      path: '/aptitudes/',
-      url: 'aptitudes.html',
-    },
-    {
-      path: '/aptitudesrec/',
-      url: 'aptitudesrec.html',
-    },
-
+    { path: '/login/', url: 'login.html', }, { path: '/index/', url: 'index.html', }, { path: '/registro/', url: 'registro.html', },
+    { path: '/confirmacion/', url: 'confirmacion.html', }, { path: '/errorreg/', url: 'errorreg.html', },
+    { path: '/errorlog/', url: 'errorlog.html', }, { path: '/datosperf/', url: 'datosperf.html', }, { path: '/regdev/', url: 'regdev.html', },
+    { path: '/regrec/', url: 'regrec.html', }, { path: '/aptitudes/', url: 'aptitudes.html', }, { path: '/aptitudesrec/', url: 'aptitudesrec.html', },
+    { path: '/busquedadevs/', url: 'busquedadevs.html', }, { path: '/busquedarecs/', url: 'busquedarecs.html', },
   ]
   // ... other parameters
 });
-
 /**GLOBALES*/
-/*db firestore*/
 var db = firebase.firestore();
-
+var tipoCat = ""; inicio = 0; mostrar = ''; userm = '';
+var nombre = ""; apellido = ""; email = "";
 /*colecciones*/
 var coleccionUsuarios = db.collection("Usuarios");
 var coleccionLenguajesd = db.collection("Lenguajesd");
 var coleccionLenguajesr = db.collection("Lenguajesr");
 
-/*varias*/
-var tipoCat = ""; inicio = 0; mostrar = '';
-var nombre = ""; apellido = ""; email = "";
-
-
-
-
 var mainView = app.views.create('.view-main');
 
-// Handle Cordova Device Ready Event
 $$(document).on('deviceready', function () {
   console.log("Device is ready!");
 });
-
-
 $$(document).on('page:init', function (e) {
-  // Do something here when page loaded and initialized
   console.log(e);
 })
 
-$$(document).on('page:init', '.page[data-name="index"]', function (e) {
-  // Do something here when page with data-name="login" attribute loaded and initialized
-  console.log('ingreso al index');
-  agregarlenguajes();
+$$(document).on('page:init', '.page[data-name="index"]', function (e) { /* console.log('ingreso al index');*/
+  /*agregarlenguajes();*/
 })
-
 
 $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
-  // Do something here when page with data-name="login" attribute loaded and initialized
   console.log(e);
 })
-
 
 $$(document).on('page:init', '.page[data-name="regdev"]', function (e) {
-  // Do something here when page with data-name="login" attribute loaded and initialized
-  console.log(e);
   $$('#regDev').on('click', registroDev)
-
-
 })
-
 
 $$(document).on('page:init', '.page[data-name="regrec"]', function (e) {
-  // Do something here when page with data-name="login" attribute loaded and initialized
-  console.log(e);
   $$('#regRec').on('click', registroRec)
-
-
 })
-
 
 $$(document).on('page:init', '.page[data-name="login"]', function (e) {
-  // Do something here when page with data-name="login" attribute loaded and initialized
-  console.log(e);
   $$('#log').on('click', login);
-
 })
 
-
-
-
+$$(document).on('page:init', '.page[data-name="busquedadevs"]', function (e) {
+  
+ 
+})
 
 $$(document).on('page:init', '.page[data-name="aptitudes"]', function (e) {
-  // Do something here when page with data-name="login" attribute loaded and initialized
-  console.log(e);
   fncargardatos();
   $$('#sumalenguaD').on('click', fnsumateested);
-
-
 
   coleccionLenguajesd.orderBy("categoria")
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-
-
         if (tipoCat != doc.data().categoria) {
-          mostrar += `
-              
-              <div class="block-title">`+ doc.data().categoria + `</div>
-              
+          mostrar += `              
+              <div class="block-title">`+ doc.data().categoria + `</div>              
               <li>
               <label class="item-radio item-radio-icon-start item-content">
                 <input type="radio" name="demo-radio-start"/>
@@ -164,13 +85,9 @@ $$(document).on('page:init', '.page[data-name="aptitudes"]', function (e) {
                 </div>
               </label>
             </li>
-              
-              
               `;
           tipoCat = doc.data().categoria;
-
         }
-
         else {
           mostrar += `<label class="item-radio item-radio-icon-start item-content">
                 <input type="radio" name="demo-radio-start"/>
@@ -180,119 +97,75 @@ $$(document).on('page:init', '.page[data-name="aptitudes"]', function (e) {
                 </div>
               </label>
             </li>
-            
-            
             `
-
         }
-        /*
-                n=doc.data().lenguaje;*/
-
         console.log(doc.id, " => ", doc.data().tipo, "  /  ", doc.data().lenguaje);
       });
-
       $$('#listalenguajes').append(mostrar);
     })
     .catch((error) => {
       console.log("Error getting documents: ", error);
     });
-
-
-
 })
 
-
 $$(document).on('page:init', '.page[data-name="aptitudesrec"]', function (e) {
-  // Do something here when page with data-name="login" attribute loaded and initialized
-  console.log(e);
-
   fncargardatosrec();
-
-
+  $$('#sumalenguaR').on('click', fnsumateester);
 
   coleccionLenguajesr.orderBy("categoria")
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-
-
         if (tipoCat != doc.data().categoria) {
-          mostrar += `
-              
-              <div class="block-title">`+ doc.data().categoria + `</div>
-              
+          mostrar += `              
+              <div class="block-title">`+ doc.data().categoria + `</div>              
               <li>
               <label class="item-radio item-radio-icon-start item-content">
                 <input type="radio" name="demo-radio-start"/>
                 <i class="icon icon-radio"></i>
                 <div class="item-inner">
-                  <div class="item-title">`+ doc.data().lenguaje + `</div>
+                <div id="${doc.data().lenguaje}" class="item-title" onclick="seteaLengR(${doc.data().lenguaje})">${doc.data().lenguaje}</div>
                 </div>
               </label>
             </li>
-              
-              
               `;
           tipoCat = doc.data().categoria;
-
         }
-
         else {
           mostrar += `<label class="item-radio item-radio-icon-start item-content">
                 <input type="radio" name="demo-radio-start"/>
                 <i class="icon icon-radio"></i>
                 <div class="item-inner">
-                  <div class="item-title">`+ doc.data().lenguaje + `</div>
+                <div id="${doc.data().lenguaje}" class="item-title" onclick="seteaLengR(${doc.data().lenguaje})">${doc.data().lenguaje}</div>
                 </div>
               </label>
-            </li>`
+            </li>
+            `
         }
-
-
-
         console.log(doc.id, " => ", doc.data().tipo, "  /  ", doc.data().lenguaje);
       });
-
       $$('#listalenguajesr').append(mostrar);
     })
     .catch((error) => {
       console.log("Error getting documents: ", error);
     });
-
-
 })
-
-
-
 
 /**REGISTRO */
 
 /*Dev reg*/
-
 function registroDev() {
 
-
-  email = $$('#regmail').val();
-  nombre = $$('#regnomb').val();
-  apellido = $$('#regapell').val();
-  password = $$('#regcontra').val();
-
-
-
+  email = $$('#regmail').val(); nombre = $$('#regnomb').val(); apellido = $$('#regapell').val(); password = $$('#regcontra').val();
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      // Signed in
       mainView.router.navigate('/confirmacion/');
-
       var datosDevs = {
         nombre: nombre,
         apellido: apellido,
         tipoUsuario: "Desarrollador"
       }
-
-
       coleccionUsuarios.doc(email).set(datosDevs)
         .then(function () {
           console.log("Base de dato ok");
@@ -300,11 +173,7 @@ function registroDev() {
         .catch(function (error) {
           console.log("Error" + error);
         });
-
-
-
     })
-
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -315,44 +184,25 @@ function registroDev() {
 
       if (errorCode == "auth/email-already-in-use") {
         console.error("el mail ya esta usado");
-
-
       }
-
-
     });
-
 }
 
 /*Dev reg*/
-
-
 /*recruiter reg*/
 
 function registroRec() {
 
-  email = $$('#regemail').val();
-  nombre = $$('#regemp').val();
-  apellido = $$('#regempape').val();
-
-  password = $$('#regecontra').val();
-
-
-
+  email = $$('#regemail').val(); nombre = $$('#regemp').val(); apellido = $$('#regempape').val(); password = $$('#regecontra').val();
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      // Signed in
       mainView.router.navigate('/confirmacion/');
-
       var datosRecruit = {
         nombre: nombre,
         apellido: apellido,
         tipoUsuario: "Recruiter"
       }
-
-
-
       coleccionUsuarios.doc(email).set(datosRecruit)
         .then(function () {
           console.log("Registro Rec ok");
@@ -360,12 +210,7 @@ function registroRec() {
         .catch(function (error) {
           console.log("Error" + error);
         });
-
-
-
-
     })
-
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -376,60 +221,36 @@ function registroRec() {
 
       if (errorCode == "auth/email-already-in-use") {
         console.error("el mail ya esta usado");
-
-
       }
-
-
     });
-
 }
 
 /*recruiter reg*/
-
 /**REGISTRO */
-
-
 
 /**LOGIN */
 
 function login() {
 
-  var email = $$('#maillog').val();
-  var password = $$('#contralog').val();
-
-
+  var email = $$('#maillog').val(); var password = $$('#contralog').val();
 
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      // Signed in
       var user = userCredential.user;
-
       console.log("Bienvenid@!!! " + email);
-      // traer los datos de la base de datos de ESTE usuario en particular
-
       docRef = coleccionUsuarios.doc(email)
-
       docRef.get().then((doc) => {
         if (doc.exists) {
           console.log("Document data:", doc.data());
           tipoUsuario = doc.data().tipoUsuario;
-
-
           if (tipoUsuario == "Desarrollador") {
             console.log("anda para Desarrollador");
             mainView.router.navigate('/aptitudes/');
           } else {
             console.log("vamos para el Recruiter");
             mainView.router.navigate('/aptitudesrec/');
-
-
           }
-
-
-
         } else {
-          // doc.data() will be undefined in this case
           console.log("No such document!");
         }
       }).catch((error) => {
@@ -438,20 +259,14 @@ function login() {
         console.error(errorMessage);
         mainView.router.navigate('/errorlog/');
       });
-
     });
 }
-
 
 /**LOGIN */
-
-
-
-
+ /*
 function agregarlenguajes() {
-  /*
-    id = "1"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"Python", usuarios:[]};
-  
+ 
+    id = "1"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"Python", usuarios:[]};  
     coleccionLenguajesd.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
@@ -459,8 +274,7 @@ function agregarlenguajes() {
     .catch(function(error) {
       console.log("Error" + error);
     });
-    id = "2"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"Java", usuarios:[]};
-  
+    id = "2"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"Java", usuarios:[]};  
     coleccionLenguajesd.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
@@ -468,8 +282,7 @@ function agregarlenguajes() {
     .catch(function(error) {
       console.log("Error" + error);
     });
-    id = "3"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"php", usuarios:[]};
-  
+    id = "3"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"php", usuarios:[]};  
     coleccionLenguajesd.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
@@ -477,8 +290,7 @@ function agregarlenguajes() {
     .catch(function(error) {
       console.log("Error" + error);
     });
-    id = "4"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"Angular", usuarios:[]};
-  
+    id = "4"; datos = { categoria: "lenguajesd", tipo: "dev", lenguaje:"Angular", usuarios:[]};  
     coleccionLenguajesd.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
@@ -486,8 +298,7 @@ function agregarlenguajes() {
     .catch(function(error) {
       console.log("Error" + error);
     });
-    id = "1"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"Python", usuarios:[]};
-  
+    id = "1"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"Python", usuarios:[]};  
     coleccionLenguajesr.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
@@ -495,8 +306,7 @@ function agregarlenguajes() {
     .catch(function(error) {
       console.log("Error" + error);
     });
-    id = "2"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"Java", usuarios:[]};
-  
+    id = "2"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"Java", usuarios:[]};  
     coleccionLenguajesr.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
@@ -504,8 +314,7 @@ function agregarlenguajes() {
     .catch(function(error) {
       console.log("Error" + error);
     });
-    id = "3"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"php", usuarios:[]};
-  
+    id = "3"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"php", usuarios:[]};  
     coleccionLenguajesr.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
@@ -513,23 +322,19 @@ function agregarlenguajes() {
     .catch(function(error) {
       console.log("Error" + error);
     });
-    id = "4"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"Angular", usuarios:[]};
-  
+    id = "4"; datos = { categoria: "lenguajesr", tipo: "recruiter", lenguaje:"Angular", usuarios:[]};  
     coleccionLenguajesr.doc(id).set(datos)
     .then(function() {
       console.log("nuevo lenguaje");
     })
     .catch(function(error) {
       console.log("Error" + error);
-    });*/
-}
+    });
+}*/
 
 /**LENGUAJES */
 
-
 function fncargardatos() {
-
-
 
   docRef.get().then((doc) => {
     if (doc.exists) {
@@ -538,13 +343,11 @@ function fncargardatos() {
       $$('#nomperf').html("Nombre:  " + doc.data().nombre);
       $$('#apelperf').html("Apellido:  " + doc.data().apellido);
     } else {
-      // doc.data() will be undefined in this case
       console.log("No such document!");
     }
   }).catch((error) => {
     console.log("Error getting document:", error);
   });
-
 }
 
 function fncargardatosrec() {
@@ -556,42 +359,27 @@ function fncargardatosrec() {
       $$('#nomperfrec').html("Nombre:  " + doc.data().nombre);
       $$('#apelperfrec').html("Apellido:  " + doc.data().apellido);
     } else {
-      // doc.data() will be undefined in this case
       console.log("No such document!");
     }
   }).catch((error) => {
     console.log("Error getting document:", error);
   });
-
 }
-
- 
 
 var lenguajeSeteado = "";
 function seteaLeng(lenguaje) {
-lenguajeSeteado = lenguaje;
+  lenguajeSeteado = lenguaje;
+}
+function fnsumateested() {
+  $$('#nwleng').append(lenguajeSeteado);
 }
 
-function fnsumateested() {
-
-$$('#nwleng').append(lenguajeSeteado);
-
-  /*
-  var docRef = coleccionLenguajesd.doc("1");
+var lenguajeSeteador = "";
+function seteaLengR(lenguaje) {
+  lenguajeSeteador = lenguaje;
+}
+function fnsumateester() {
+  $$('#nwlengr').append(lenguajeSeteador);
   
-  docRef.get().then((doc) => {
-  if (doc.exists) {
-  console.log("Document data:", doc.data());
-  console.log(doc.data().lenguaje);
-  $$('#nwleng').html(doc.data().lenguaje);
-  } else {
-  // doc.data() will be undefined in this case
-  console.log("No such document!");
-  }
-  }).catch((error) => {
-  console.log("Error getting document:", error);
-  });
-  */
-
 
 }
